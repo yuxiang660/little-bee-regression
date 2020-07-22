0fddbc093231c50a1686db5fcdd7da76668299c0
Ben <yuxiang660@163.com>
2020-07-22 22:48:35 +0800

    add configs

--- tester/sequencer.py
+++ tester/sequencer.py
@@ -14,12 +14,12 @@ class Sequencer:
             self._jobs[test_name] = Job(test_name, test_dir, log_file)
             self._job_log_files[test_name] = log_file
 
-    def run(self):
+    def run(self, compile_configs, run_configs):
         for job_name, job in self._jobs.items():
             logging.info(f"### Start test '{job_name}' ###")
             job.clean()
-            job.compile([])
-            job.run([])
+            job.compile(compile_configs)
+            job.run(run_configs)
             run_time = job.get_runtime()
             logging.info("### End test '{}' in {:.3f} seconds ###".format(
                 job_name, run_time))
--- tests/sequencer_tests.py
+++ tests/sequencer_tests.py
@@ -9,12 +9,13 @@ test_root_path = './mock'
 test_names = ["test1", "test2"]
 log_dir = './output'
 regression_log = './output/regression.log'
-
+compile_configs = ['echo "set up compile environment1"', 'echo "set up compile environment2"']
+run_configs = ['echo "set up run environment1"', 'echo "set up run environment2"']
 
 class SequencerTestSuite(unittest.TestCase):
     def test_sequencer_run(self):
         seq = Sequencer(test_root_path, test_names, log_dir)
-        seq.run()
+        seq.run(compile_configs, run_configs)
         seq.log(regression_log)
 
 
