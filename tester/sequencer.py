960cd9b075373c86f86a6918e23bbb969b1cddf9
Ben <yuxiang660@163.com>
2020-07-22 22:42:20 +0800

    add regression log

--- tester/sequencer.py
+++ tester/sequencer.py
@@ -7,10 +7,12 @@ class Sequencer:
     def __init__(self, test_root_path, test_names, log_dir):
         os.makedirs(log_dir, exist_ok=True)
         self._jobs = {}
+        self._job_log_files = {}
         for test_name in test_names:
             test_dir = test_root_path + f'/{test_name}'
             log_file = log_dir + f'/{test_name}.log'
             self._jobs[test_name] = Job(test_name, test_dir, log_file)
+            self._job_log_files[test_name] = log_file
 
     def run(self):
         for job_name, job in self._jobs.items():
@@ -21,3 +23,8 @@ class Sequencer:
             run_time = job.get_runtime()
             logging.info("### End test '{}' in {:.3f} seconds ###".format(
                 job_name, run_time))
+    
+    def log(self, log_file):
+        assert os.path.exists(os.path.dirname(log_file))
+        with open(log_file, 'w') as f:
+            f.write("Regression Test Results:\n")
--- tests/sequencer_tests.py
+++ tests/sequencer_tests.py
@@ -8,12 +8,14 @@ logging.basicConfig(level=logging.DEBUG,
 test_root_path = './mock'
 test_names = ["test1", "test2"]
 log_dir = './output'
+regression_log = './output/regression.log'
 
 
 class SequencerTestSuite(unittest.TestCase):
     def test_sequencer_run(self):
         seq = Sequencer(test_root_path, test_names, log_dir)
         seq.run()
+        seq.log(regression_log)
 
 
 def main():
