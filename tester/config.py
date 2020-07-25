f2e20a8374841f8e956280316379833300bedd2a
Ben <yuxiang660@163.com>
2020-07-25 11:51:20 +0800

    add timeout and emulator configs

--- tester/config.py
+++ tester/config.py
@@ -2,6 +2,8 @@ import json
 import os
 
 class Config:
+    NONE_TIMEOUT = -1
+
     CASE_LIST_KEY = "case_list"
     ENV_KEY = "env"
     ENV_PATH_KEY = "path"
@@ -9,6 +11,8 @@ class Config:
     TEST_ROOT_KEY = "test_root"
     TEST_RESULT_ROOT_KEY = "test_result_root"
     LOG_FILE_KEY = "log_file"
+    EMULATOR_KEY = "emulator"
+    TIMEOUT_KEY = "timeout"
 
     def __init__(self, config_file):
         assert os.path.exists(config_file)
@@ -20,6 +24,8 @@ class Config:
         self._test_root = data[self.TEST_ROOT_KEY]
         self._test_result_root = data[self.TEST_RESULT_ROOT_KEY]
         self._log_file = data[self.LOG_FILE_KEY]
+        self._emulator = data[self.EMULATOR_KEY]
+        self._timeout = data[self.TIMEOUT_KEY]
 
     def get_test_names(self):
         return self._case_list
@@ -33,10 +39,14 @@ class Config:
     def get_log_file_path(self):
         return self._log_file
 
+    def get_timeout(self):
+        return self._timeout if self._timeout != self.NONE_TIMEOUT else None
+
     def get_env_configs(self):
         configs = []
         for path in self._env_path:
             configs.append(f'setenv PATH {path}:$PATH')
         for custom in self._env_custom:
             configs.append(custom)
+        configs.append(f'setenv EMULATOR {self._emulator}')
         return configs
--- tester/serial_tester.py
+++ tester/serial_tester.py
@@ -1,5 +1,6 @@
 from .job import Job
 from .logger import Logger
+from .config import Config
 import os
 import logging
 
@@ -7,11 +8,11 @@ import logging
 class SerialTester:
     def __init__(self, configs):
         os.makedirs(configs.get_test_result_root(), exist_ok=True)
+        self._configs = configs
+
         self._jobs = {}
         self._job_result_files = {}
         self._job_run_times = {}
-        self._job_env_configs = configs.get_env_configs()
-        self._tester_log = configs.get_log_file_path()
         for test_name in configs.get_test_names():
             test_dir = configs.get_test_root() + f'/{test_name}'
             test_result_file = configs.get_test_result_root() + f'/{test_name}.log'
@@ -21,17 +22,21 @@ class SerialTester:
     def one_click_run(self):
         for job_name, job in self._jobs.items():
             logging.info(f"### Setup test '{job_name}' ###")
-            job.setup(self._job_env_configs)
+            job.setup(self._configs.get_env_configs())
+
             logging.info(f"### Compile test '{job_name}' ###")
             job.compile()
+
             logging.info(f"### Run test '{job_name}' ###")
-            job.run()
+            job.run(self._configs.get_timeout())
             runtime = job.get_runtime()
+            logging.info(
+                "### End test '{}' in {:.3f} seconds ###".format(job_name, runtime))
+
             self._job_run_times[job_name] = runtime
-            logging.info("### End test '{}' in {:.3f} seconds ###".format(
-                job_name, runtime))
 
     def log(self):
-        os.makedirs(os.path.dirname(self._tester_log), exist_ok=True)
+        os.makedirs(os.path.dirname(
+            self._configs.get_log_file_path()), exist_ok=True)
         l = Logger(self._job_result_files, self._job_run_times)
-        l.log(self._tester_log)
+        l.log(self._configs.get_log_file_path())
