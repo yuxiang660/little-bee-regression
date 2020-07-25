842ab7dc2f2414ab07a5116e2ffcd56c9f8845dc
Ben <yuxiang660@163.com>
2020-07-25 10:42:03 +0800

    clean up configs to Config class

--- tester/serial_tester.py
+++ tester/serial_tester.py
@@ -5,29 +5,31 @@ import logging
 
 
 class SerialTester:
-    def __init__(self, test_names, test_root, test_result_root):
-        os.makedirs(test_result_root, exist_ok=True)
+    def __init__(self, configs):
+        os.makedirs(configs.get_test_result_root(), exist_ok=True)
         self._jobs = {}
         self._job_result_files = {}
         self._job_run_times = {}
-        for test_name in test_names:
-            test_dir = test_root + f'/{test_name}'
-            test_result_file = test_result_root + f'/{test_name}.log'
+        self._job_env_configs = configs.get_env_configs()
+        self._tester_log = configs.get_log_file_path()
+        for test_name in configs.get_test_names():
+            test_dir = configs.get_test_root() + f'/{test_name}'
+            test_result_file = configs.get_test_result_root() + f'/{test_name}.log'
             self._jobs[test_name] = Job(test_name, test_dir, test_result_file)
             self._job_result_files[test_name] = test_result_file
 
-    def run(self, configs):
+    def one_click_run(self):
         for job_name, job in self._jobs.items():
             logging.info(f"### Start compile test '{job_name}' ###")
-            job.compile(configs)
+            job.compile(self._job_env_configs)
             logging.info(f"### Start running test '{job_name}' ###")
-            job.run(configs)
+            job.run(self._job_env_configs)
             runtime = job.get_runtime()
             self._job_run_times[job_name] = runtime
             logging.info("### End test '{}' in {:.3f} seconds ###".format(
                 job_name, runtime))
 
-    def log(self, output_file):
-        os.makedirs(os.path.dirname(output_file), exist_ok=True)
+    def log(self):
+        os.makedirs(os.path.dirname(self._tester_log), exist_ok=True)
         l = Logger(self._job_result_files, self._job_run_times)
-        l.log(output_file)
+        l.log(self._tester_log)
--- tests/serial_tester_tests.py
+++ tests/serial_tester_tests.py
@@ -1,23 +1,18 @@
 from . import context
-from tester import SerialTester
+from tester import SerialTester, Config
 import logging
 import unittest
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(levelname)s - %(asctime)s - %(message)s')
 
-test_names = ["test1", "test2"]
-test_root = './mock'
-test_result_root = './output'
-log_file = './output/regression.log'
-configs = ['echo "set up environment1"', 'echo "set up environment2"']
-
+config_file = "config.json"
 
 class SequencerTestSuite(unittest.TestCase):
     def test_sequencer_run(self):
-        s = SerialTester(test_names, test_root, test_result_root)
-        s.run(configs)
-        s.log(log_file)
+        s = SerialTester(Config(config_file))
+        s.one_click_run()
+        s.log()
 
 
 def main():
